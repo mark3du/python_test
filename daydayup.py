a49da97781b63cbb51eb7ca760de5709180cdc8a
mark3du <13611029026.com>
2023-02-01 22:03:44 +0800

    daydayup original

---
+++ daydayup.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Feb  9 17:46:25 2020
+
+@author: 杜敬祎
+"""
+a=eval(input("输入："))
+dayup=(1+a)**365
+daydown=(1-a)**365
+print("向上：{:.2f},向下：{:.2f}".format(dayup,daydown))
